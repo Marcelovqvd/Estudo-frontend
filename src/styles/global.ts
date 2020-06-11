c88f7b4b79fa34188d6c15f86840deb99a4f59cd
Marcelo <marcelovqvd@gmail.com>
2020-06-11 14:41:04 -0300

    change global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,22 +9,22 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #F5F5F5;
+    background: #312e38;
+    color: #FFF;
     -webkit-font-smoothing: antialiased;
   }
 
   body, input, button {
-    font: 16px Roboto, sans-serif;
-  }
-
-  #root {
-    max-width: 960px;
-    margin: 0 auto;
-    padding: 40px 20px;
+    font-family: 'Roboto', sans-serif;
+    font: 16px;
   }
 
   button {
     cursor: pointer;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
   }
 `;
 
